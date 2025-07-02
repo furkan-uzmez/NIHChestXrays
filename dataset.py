9e3f300abc7ad9aae70b2757323c33bddae979a9
Furkan <furkan.n41@hotmail.com>
2025-07-02 12:22:46 +0300

    split train-test dataset

--- dataset.py
+++ dataset.py
@@ -2,24 +2,21 @@ from torch.utils.data import Dataset
 from PIL import Image
 import os
 
-class CustomImageDataset(Dataset):
-    def __init__(self, image_paths, labels=None, transform=None):
-        self.image_paths = image_paths
-        self.labels = labels  
+class ChestXrayDataset(Dataset):
+    def __init__(self, df, transform=None):
+        self.df = df
         self.transform = transform    
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
+        img_path = self.df.iloc[idx]['image_path']
+        label = self.df[idx,'View_Position']
+
         image = Image.open(img_path).convert("RGB")
 
         if self.transform:
             image = self.transform(image)
 
-        if self.labels is not None:
-            label = self.labels[idx]
-            return image, label
-        else:
-            return image
+        return image, label
